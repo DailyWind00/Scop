b915de810f0874a61fdd105f103dc0399ee94e54
DailyWind00 <gallaismael0@gmail.com>
2024-10-02 01:52:36 +0200

    Added events handling function in the routine

--- srcs/routine.cpp
+++ srcs/routine.cpp
@@ -1,25 +1,35 @@
 #include "config.hpp"
 
+// Handle all keyboard & other events
+static void	handleEvents(GLFWwindow *window) {
+	glfwPollEvents();
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// Keep the window alive, exiting this function mean the process is over
 static void program_loop(GLFWwindow *window, OBJ &obj, unsigned int shader) {
+	
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glUseProgram(shader);
-		glfwPollEvents();
+		handleEvents(window);
 	}
+
 	(void)obj;
 }
 
 // Will call the program loop
 void	RenderObject(GLFWwindow *window, OBJ &obj) {
 
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-
-	unsigned int shader = make_shader( // Will probably move to CreateWindow
+	unsigned int shader = make_shader(
 		"./srcs/shaders/vertex.vert",
 		"./srcs/shaders/fragment.frag"
 	);
 
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	program_loop(window, obj, shader);
-	glDeleteProgram(shader); // Will probably move to DestroyWindow
+	glDeleteProgram(shader);
 }
