817cd3a461f7753a878b46010cc9816098555d2e
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-10-02 15:42:33 +0200

    First triangle in OpenGL

--- includes/config.hpp
+++ includes/config.hpp
@@ -23,7 +23,7 @@ GLFWwindow		*CreateWindow(OBJ &obj);
 void			DestroyWindow(GLFWwindow *window);
 
 // shader.cpp
-GLuint 	make_shader(const string &vertex_path, const string &fragment_path);
+GLuint 			make_shader(const string &vertex_path, const string &fragment_path);
 
 // routine.cpp
 void			RenderObject(GLFWwindow *window, OBJ &obj);
--- srcs/routine.cpp
+++ srcs/routine.cpp
@@ -11,13 +11,39 @@ static void	handleEvents(GLFWwindow *window) {
 // Keep the window alive, exiting this function mean the process is over
 static void program_loop(GLFWwindow *window, OBJ &obj, GLuint shader) {
 
+	// tests to remove
+	static const GLfloat g_vertex_buffer_data[] = {
+		-0.5f, -0.5f, 0.0f, 
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.75f, 0.0f,
+	};
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	// ---
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glfwSwapBuffers(window);
+		
 		glUseProgram(shader);
-		handleEvents(window);
-	}
+		glBindVertexArray(vertexbuffer);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		handleEvents(window);
+		glfwSwapBuffers(window);
+	}
+	glDisableVertexAttribArray(0);
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
 	(void)obj;
 }
 
