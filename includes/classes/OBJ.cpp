d1545a8524aa4aaa6ba15f50173752a9ef23da31
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-10-28 10:38:33 +0100

    Material implementation WIP, shape have materials

--- includes/classes/OBJ.cpp
+++ includes/classes/OBJ.cpp
@@ -114,22 +114,22 @@ void	OBJ::parseOBJ(const string &file_name) {
 				throw runtime_error("Error while parsing face at line " + to_string(i) + " : invalid number of vertices");
 		}
 		else if (type == "mtllib") {
-			string mtl_file_name;
-			iss >> mtl_file_name;
+			string mtl_path;
+			iss >> mtl_path;
 			if (!iss)
 				throw runtime_error("Error while parsing material file at line " + to_string(i));
-			if (mtl_file_name.size() < 4 || mtl_file_name.substr(mtl_file_name.size() - 4) != ".mtl")
+			if (mtl_path.size() < 4 || mtl_path.substr(mtl_path.size() - 4) != ".mtl")
 				throw runtime_error("Error while opening material file : invalid extension");
 
-			mtl_file_name = file_name.substr(0, file_name.find_last_of("/\\") + 1) + mtl_file_name; // get the path of the mtl file
+			mtl_path = file_name.substr(0, file_name.find_last_of("/\\") + 1) + mtl_path; // get the path of the mtl file
 
 			try {
-				printVerbose("Parsing material file : " + mtl_file_name);
-				// parseMTL(mtl_file_name);
+				printVerbose("Parsing material file : " + mtl_path);
+				parseMTL(mtl_path);
 				printVerbose("Material file parsed");
 			}
 			catch(const std::exception& e) {
-				throw runtime_error("Error in " + mtl_file_name + " : " + e.what());
+				throw runtime_error("Error in " + mtl_path + " : " + e.what());
 			}
 		}
 		else if (type == "usemtl") {
@@ -137,10 +137,24 @@ void	OBJ::parseOBJ(const string &file_name) {
 			iss >> material_name;
 			if (!iss)
 				throw runtime_error("Error while parsing material name at line " + to_string(i));
-			// if (obj.materials.find(material_name) == obj.materials.end()) {
-			// 	cout << BOrange << "Warning : material " << material_name << " not found at line " << i << ResetColor << endl;
-			// 	continue;
-			// }
+
+			auto it = obj.materials.begin();
+			while (it->name != material_name && it != obj.materials.end())
+				it++;
+
+			if (it == obj.materials.end()) {
+				cout << BOrange << "Warning : material \"" << material_name << "\" not found at line " << i << ResetColor << endl;
+				continue;
+			}
+
+			if (obj.shapes.empty()) { // Create a default group if no group is defined
+				cout << BYellow << "Notice : no group defined at line " << i << ", creating a default group" << ResetColor << endl;
+				Shape shape; shape.name = "default";
+				obj.shapes.push_back(shape);
+			}
+
+			obj.shapes.back().material = *it;
+			printVerbose("Shape " + obj.shapes.back().name + " is now using material " + obj.shapes.back().name);
 		}
 	}
 	if (obj.name.empty())
@@ -155,44 +169,42 @@ void	OBJ::parseOBJ(const string &file_name) {
 	}
 }
 
-// void	OBJ::parseMTL(const string &object_file_path) {
-// 	ifstream	object_file = openReadFile(object_file_path);
-// 	string	line;
-// 	int		i = 0;
-// 	Material current_material;
+void	OBJ::parseMTL(const string &object_file_path) {
+	ifstream	object_file = openReadFile(object_file_path);
+	string	line;
+	int		i = 0;
+	Material current_material;
 
-// 	while (getline(object_file, line)) {
-// 		istringstream iss(line);
-// 		string type;
-// 		iss >> type;
-// 		i++;
+	while (getline(object_file, line)) {
+		istringstream iss(line);
+		string type;
+		iss >> type;
+		i++;
 
-// 		if (type == "newmtl") {
-// 			if (!current_mtl.material.empty()) {
-// 				obj.materials.insert(pair<string, mtl_Data>(current_mtl.material, current_mtl));
-// 				current_mtl = {}; // reset the current material
-// 			}
-// 			string material_name;
-// 			iss >> material_name;
-// 			if (!iss)
-// 				throw runtime_error("Error while parsing material name at line " + to_string(i));
-// 			current_mtl.material = material_name;
-// 		}
-// 		else if (type == "map_Kd") {
-// 			string texture_path;
-// 			iss >> texture_path;
-// 			if (!iss)
-// 				throw runtime_error("Error while parsing texture path at line " + to_string(i));
-// 			current_mtl.texture_path = object_file_path.substr(0, object_file_path.find_last_of("/\\") + 1) + texture_path; // get the path of the texture file
-// 		}
-// 	}
-// 	if (!current_mtl.material.empty())
-// 		obj.materials.insert(pair<string, mtl_Data>(current_mtl.material, current_mtl));
-// 	object_file.close();
-// 	printVerbose(to_string(obj.materials.size()) + " materials loaded");
-// 	for (const auto &mtl : obj.materials)
-// 		printVerbose("Material : " + mtl.first + " - Texture : " + mtl.second.texture_path);
-// }
+		if (type == "newmtl") {
+			if (!current_material.name.empty()) {
+				obj.materials.push_back(current_material);
+				current_material = {}; // reset the current material
+			}
+			iss >> current_material.name;
+			if (!iss)
+				throw runtime_error("Error while parsing material name at line " + to_string(i));
+		}
+		else if (type == "map_Kd") {
+			string path;
+			iss >> path;
+			if (!iss)
+				throw runtime_error("Error while parsing texture path at line " + to_string(i));
+			path = object_file_path.substr(0, object_file_path.find_last_of("/\\") + 1) + path; // get the path of the texture file
+			current_material.texture_path = path;
+		}
+	}
+	obj.materials.push_back(current_material);
+	object_file.close();
+	printVerbose(to_string(obj.materials.size()) + " materials loaded");
+	for (const Material &mtl : obj.materials)
+		printVerbose("Material : " + mtl.name + " - Texture : " + mtl.texture_path);
+}
 
 void	OBJ::useTexture(const string &texture_path) {
 	int width, height, nrChannels;
@@ -270,10 +282,8 @@ void	OBJ::setBuffers() {
 	int color = 0;
 
 	for (const Shape &shape : obj.shapes) {
-		cout << "Shape : " << shape.name << endl;
-		cout << "Indices : " << shape.indices.size() << endl;
+		printVerbose("Shape : " + shape.name + " - Indices : " + to_string(shape.indices.size()));
 		for (const Indice &indice : shape.indices) {
-			cout << "Indice : " << indice[0] << " " << indice[1] << " " << indice[2] << endl;
 			Vertex vertex;
 			vertex.position  = {obj.attributes.positions[3 * indice[0]], obj.attributes.positions[3 * indice[0] + 1], obj.attributes.positions[3 * indice[0] + 2]};
 			vertex.color     = {DEFAULT_COLORS[color % 3], DEFAULT_COLORS[color % 3], DEFAULT_COLORS[color % 3]};
