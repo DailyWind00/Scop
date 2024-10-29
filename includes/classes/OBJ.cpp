056636e7d0f166628a720cda8e63d5d7a0349b8d
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-10-29 13:07:09 +0100

    remove debug prints

--- includes/classes/OBJ.cpp
+++ includes/classes/OBJ.cpp
@@ -115,12 +115,6 @@ void	OBJ::setBuffers() {
     std::vector<Vertex> vertexes;
 	int color = 0;
 
-	for (const Material &mat : obj.materials) {
-		cout << "Material : " << mat.name << endl;
-		cout << "Path     : " << mat.texture_path << endl;
-		cout << "Index    : " << mat.texture_index << endl;
-		cout << "Texture  : " << TBO[mat.texture_index] << endl;
-	}
 	printVerbose("Loading " + to_string(obj.shapes.size()) + " shapes :");
 	for (const Shape &shape : obj.shapes) {
 		printVerbose("| Shape : " + shape.name + " - Indices : " + to_string(shape.indices.size()));
