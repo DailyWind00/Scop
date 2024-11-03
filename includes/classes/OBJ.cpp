2195d63400f76449f39f05cfd5a3f6b6becab6fc
DailyWind00 <gallaismael0@gmail.com>
2024-11-03 04:27:14 +0100

    free loaded image if invalid nrChannel

--- includes/classes/OBJ.cpp
+++ includes/classes/OBJ.cpp
@@ -60,8 +60,10 @@ void	OBJ::setObjectTextures() {
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		else if (nrChannels == 4)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		else
+		else {
+			stbi_image_free(data);
 			throw runtime_error("Error while loading texture : invalid number of channels");
+		}
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		stbi_image_free(data);
